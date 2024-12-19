f1054b389c35799ca2d3a3c5c22987cc429d69f9
Luigy <luigy.carolino66@gmail.com>
2024-12-19 09:08:25 +0100

    Update Calc_XOR.py

--- Calc_XOR.py
+++ Calc_XOR.py
@@ -8,7 +8,7 @@ def xor_bytes(a, b):
     # Realiza el XOR
     return ''.join(f'{x ^ y:02x}' for x, y in zip(a, b))
 
-# Operación XOR 
+# Tipea inputs para el calculo de manera manual 
 if __name__ == "__main__": # Descubro el porque del __name__ == "__main__"
     input_1 = "B1EF2ACFE2BAEEFF"
     input_2 = "91BA13BA21AABB12"
